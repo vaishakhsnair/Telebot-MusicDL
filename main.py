7987bc76befd9b0276e9ebfab4889fa055970ad0
vaishakhsnair <vaishakhsnair2004@gmail.com>
2021-11-04 12:22:09 +0530

    added basic download progress and message deletion on startup

--- main.py
+++ main.py
@@ -1,55 +1,74 @@
-from telethon import TelegramClient, events,functions
-from telethon.tl.types import InputMessagesFilterMusic,Document
-import time
 import os
-api_id = 1234 # here is the api id
-api_hash =  "0123456789abcdef0123456789abcdef"   # here is the hash
+import time
+from telethon import TelegramClient
+from telethon.tl.types import InputMessagesFilterMusic
 
-
-bot = "FusionDL_Bot"
-
+api_id = 8828647
+api_hash = "61b3bd6272e586d5bd214ffb822321f4"
 
 client = TelegramClient("anon", api_id, api_hash)
 
-link = ""           # a valid deezer music link 
-
+link = "https://deezer.com/track/1398346782"
 path = "dls/"
-async def send():
+bot = "FusionDL_Bot"
 
-    message = await client.send_message(bot, link)
+
+async def send():
+    messages = await client.get_messages(bot, None, reverse=True)
+    for i in messages:
+        await client.delete_messages(bot, i)
+    await client.send_message(bot, link)
     time.sleep(5)
 
+
 completed = os.listdir(path)
+print(completed)
 message_list = []
 
+
+def progress_bar(current, total):
+    print((current / total) * 100, "%")
+
+
 async def dl():
-    messages = await client.get_messages(bot,None, reverse=True,filter = InputMessagesFilterMusic)
-    print(len(messages))
+    messages = await client.get_messages(bot, None, reverse=True, filter=InputMessagesFilterMusic)
     message_list.append(messages)
     for i in messages:
         one = i.audio
         name = one.attributes[0].title
-        print(name)
-        if name+".flac" not in completed:
-            print("Downloading :",name)
-            await client.download_media(messages[0],path+name)
+        if name + ".flac" not in completed:
+            print("Downloading :", name)
+            await client.download_media(messages[0], os.path.join(path, name), progress_callback=progress_bar)
             completed.append(name)
         else:
-            print(name,": Already existing")
-
-   
+            print(name, ": Already existing")
 
 
-    
+def change_quality(quality):
+    async def ch_qlty(quality):
+        await client.send_message(bot, "/settings")
+        message_raw = []
+        async for i in client.iter_messages(bot, reverse=True):
+            message_raw = i
+
+        print(message_raw)
+
+    ##        await message.click(0)
+    ##        async for i in client.iter_messages(bot,reverse=True,from_user=bot):
+    ##            message=i
+    ##            break
+    ##        print(message)
+    ##        await message.click(quality)
+
+    with client:
+        client.loop.run_until_complete(ch_qlty(quality))
+
+
 def runner():
     with client:
         client.loop.run_until_complete(send())
         client.loop.run_until_complete(dl())
-    
+
 
 if __name__ == "__main__":
     runner()
-
-
-
-
